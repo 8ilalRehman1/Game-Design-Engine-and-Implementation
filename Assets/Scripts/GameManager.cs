ae055c6dcc0f4ad59bb2e27562893c9df9726d55
8ilalRehman1 <145934450+8ilalRehman1@users.noreply.github.com>
2026-09-21 16:48:18 -0400

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public bool isGameOver;
+    private Enemy[] allEnemies;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        allEnemies = FindObjectsOfType<Enemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (Enemy enemy in allEnemies)
+        {
+            enemy.EnemyMove();
+        }
     }
 }
